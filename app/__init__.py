a2574c3389af0f0486e8cad7ddcb3edf9ce7d7a7
Jie Chen <jiechenmc@gmail.com>
2022-07-14 02:00:03 -0500

    migrating to prisma

--- app/__init__.py
+++ app/__init__.py
@@ -1,27 +1,6 @@
 from flask import Flask
-from peewee import *
 
 app = Flask(__name__)
-
-db = SqliteDatabase("data.db")
-print(db)
-
-
-class JobEntry(Model):
-    count: IntegerField()
-    applied: IntegerField()
-    assessed: IntegerField()
-    company: TextField()
-    position: TextField()
-    area: TextField()
-    link: TextField()
-    lastContact: TextField()
-
-    class Meta:
-        database = db
-
-
-db.create_tables([JobEntry], safe=True)
 
 
 @app.route("/api")
